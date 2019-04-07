609d3809fa7ef0229185c3879d29c31b9638378c
evanfellman <evan.fellman@gmail.com>
2019-04-07 00:41:13 -0400

    Mark as Complete Ability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_020458) do
+ActiveRecord::Schema.define(version: 2019_04_07_040400) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_07_020458) do
     t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
